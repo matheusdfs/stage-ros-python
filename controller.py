ef86129ef77c1e861ec389563873b5e4f02465e4
matheus diniz <dfmatheusdf@gmail.com>
2022-09-23 11:26:15 -0300

    base scan data divided in quadrants

--- controller.py
+++ controller.py
@@ -1,16 +1,28 @@
-from tokenize import String
 import rospy
+import string
 
+from math import floor
 from utils import print_error
 
 from nav_msgs.msg import Odometry
 
 from sensor_msgs.msg import LaserScan
 
+NUM_QUADRANTS = 4
+
 class Controller:
-    name: String = None
+    name: string = None
+
+    # variables odometry
     odom_data: Odometry = None
-    base_scan_data: LaserScan = None
+
+    # variables sensor
+    base_scan_ranges = []
+    # quadrant_x_lowest_value_element
+    quadrant_one_lve = 0
+    quadrant_two_lve = 0
+    quadrant_three_lve = 0
+    quadrant_four_lve = 0
 
     def __init__(self, name = None):
         # for multiple instances of this node use the anonymous parameter true
@@ -19,12 +31,27 @@ class Controller:
         # initialize robot variables
         if name == None:
             print_error('Name of robot is empty')
+        elif name == '':
+            self.name == name
         else:
             self.name = '/' + name
 
         self.execute()
 
     def execute(self):
-        odom_data = rospy.Subscriber(self.name + '/odom', Odometry)
-        base_scan_data = rospy.Subscriber(self.name + '/base_scan', LaserScan)
-        pass
+        rospy.Subscriber('/odom', Odometry, callback=self.update_odom)
+        rospy.Subscriber('/base_scan', LaserScan, callback=self.update_base_scan)
+
+        while not rospy.is_shutdown():
+            pass
+
+    def update_odom(self, data):
+        self.odom_data = data
+
+    def update_base_scan(self, data):
+        self.base_scan_ranges = data.ranges
+        quad_len = floor(len(self.base_scan_ranges)/NUM_QUADRANTS)
+        self.quadrant_one_lve = min(self.base_scan_ranges   [0 : quad_len])
+        self.quadrant_two_lve = min(self.base_scan_ranges   [quad_len : 2*quad_len])
+        self.quadrant_three_lve = min(self.base_scan_ranges [2*quad_len : 3*quad_len])
+        self.quadrant_four_lve = min(self.base_scan_ranges  [3*quad_len : 4*quad_len])
--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import rospy
-from nav_msgs.msg import Odometry
-from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Twist
 from services import stageros_reset_world
 from controller import Controller
 
 if __name__ == '__main__':
     try:
         stageros_reset_world()
-        control = Controller('robot_0')
+        control = Controller('')
     except rospy.ROSInterruptException:
         pass
