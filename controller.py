f197643fffc7c9f683ed7a5835ddf25863e357d4
matheus diniz <dfmatheusdf@gmail.com>
2022-09-25 12:23:51 -0300

    starting multithreading

--- controller.py
+++ controller.py
@@ -16,7 +16,7 @@ from sensor_msgs.msg import LaserScan
 NUM_QUADRANTS = 4
 TIMEOUT = 10
 
-objective_position = [-18, 21]
+objective_position = [-31.13, 24.26]
 
 class Controller:
     name: string = None
@@ -55,8 +55,8 @@ class Controller:
             self.parameters = parameters
 
     def execute(self):
-        rospy.Subscriber('/odom', Odometry, callback=self.update_odom)
-        rospy.Subscriber('/base_scan', LaserScan, callback=self.update_base_scan)
+        rospy.Subscriber(self.name + '/odom', Odometry, callback=self.update_odom)
+        rospy.Subscriber(self.name + '/base_scan', LaserScan, callback=self.update_base_scan)
 
         while not(self.odom_updated and self.sensor_updated):
             pass
@@ -64,17 +64,18 @@ class Controller:
         start_time = time.time()
 
         while not rospy.is_shutdown():
-            # TODO replace the position of the robot and of the objective_position by the distance
-            result =    (self.parameters[0] * self.odom_position.position.x + \
-                        self.parameters[0] * self.odom_position.position.y + \
-                        self.parameters[1] * objective_position[0] + \
-                        self.parameters[1] * objective_position[1] + \
-                        self.parameters[2] * self.quadrant_one_lve + \
-                        self.parameters[3] * self.quadrant_two_lve + \
-                        self.parameters[4] * self.quadrant_three_lve + \
-                        self.parameters[5] * self.quadrant_four_lve) % 2
+            delta_x = self.odom_position.position.x - objective_position[0]
+            delta_y = self.odom_position.position.y - objective_position[1]
+            distance = math.sqrt(delta_x*delta_x + delta_y*delta_y)
 
-            pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+            # TODO replace the position of the robot and of the objective_position by the distance
+            result =    (self.parameters[0] * distance + \
+                        self.parameters[1] * self.quadrant_one_lve + \
+                        self.parameters[2] * self.quadrant_two_lve + \
+                        self.parameters[3] * self.quadrant_three_lve + \
+                        self.parameters[4] * self.quadrant_four_lve) % 2
+
+            pub = rospy.Publisher(self.name + '/cmd_vel', Twist, queue_size=10)
 
             msg = Twist()
             msg.linear.x = 1
@@ -82,15 +83,14 @@ class Controller:
 
             pub.publish(msg)
 
-            if (time.time() - start_time) > TIMEOUT:
-                delta_x = self.odom_position.position.x - objective_position[0]
-                delta_y = self.odom_position.position.y - objective_position[1]
-                distance = math.sqrt(delta_x*delta_x + delta_y*delta_y)
+            delta_time = time.time() - start_time
 
-                if distance > 10:
-                    distance = 10
+            if delta_time > TIMEOUT:
 
-                self.score = -0.1 * (time.time() - start_time) + 10 - distance + 5
+                if delta_time > 10:
+                    delta_time = 10
+
+                self.score = -0.1 * delta_time * delta_time + 10 - distance + 5
                 if distance < 0.1:
                     print_ok('BOAAAAAAA FILHAO ' + str(self.score))
                 else:
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+from concurrent.futures.thread import _worker
 import rospy
 import numpy
 import random
+import threading
 
 from controller import Controller
 from services import stageros_reset_world
 
-NUM_GENES = 5
-NUM_INPUTS = 6
+NUM_GENES = 4
+NUM_INPUTS = 5
 
 def initialize_random_array():
     return numpy.random.random(NUM_INPUTS)
@@ -36,15 +38,24 @@ if __name__ == '__main__':
         parameters_array.append(initialize_random_array())
 
     try:
+        rospy.init_node('default')
         while 1:
             print('Generation ' + str(num_generation))
+            stageros_reset_world()
+            workers = []
             for i in range(NUM_GENES):
-                rospy.init_node('default')
-                stageros_reset_world()
-                control = Controller('', parameters_array[i])
-                score_array[i] = control.execute()
+                control = Controller('robot_' + str(i), parameters_array[i]) 
+                x = threading.Thread(target=control.execute)
+                x.start()
+                workers.append(x)
+                # control = Controller('robot_' + str(i), parameters_array[i])
+                # score_array[i] = control.execute()
+
+            for w in workers:
+                w.join()
 
             bs = get_position_best_score(score_array)
+            print('best parent in this generation is: ' + str(bs))
 
             #select
             parameters_array[0] = parameters_array[bs]
@@ -52,11 +63,8 @@ if __name__ == '__main__':
             #mutate
             for i in range(NUM_GENES - 1):
                 parameters_array[i + 1] = parameters_array[0]
-                parameters_array[i + 1][random.randint(0, 5)] = random.random()
+                parameters_array[i + 1][random.randint(0, NUM_INPUTS - 1)] = random.random()
 
             num_generation += 1
-
-            
-
     except rospy.ROSInterruptException:
         pass
